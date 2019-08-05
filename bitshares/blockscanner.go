ffa1d9a7ef9cd78f59e87bc38331ba7c5fff580c
fanyinghao <fanyinghao@gmail.com>
2019-08-05 18:56:09 +0800

    单线程扫块

--- bitshares/blockscanner.go
+++ bitshares/blockscanner.go
@@ -32,7 +32,7 @@ import (
 const (
 	blockchainBucket = "blockchain" // blockchain dataset
 	//periodOfTask      = 5 * time.Second // task interval
-	maxExtractingSize = 10 // thread count
+	maxExtractingSize = 1 // thread count
 )
 
 //BtsBlockScanner BTS block scanner
